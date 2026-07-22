e8bb16841e0f3769aec01792b50583f960f2bc4b
med2004x <mohamedelhammi99@gmail.com>
2026-07-22 10:00:33 +0200

    g

--- functions-methods-exercises/08-function-composition/main.go
+++ functions-methods-exercises/08-function-composition/main.go
@@ -10,10 +10,31 @@ func trimUser(username []string) {
 }
 
 func isLongEnough(username []string) bool {
-	if len(username) != 0 {
+	if len(username) >= 4 {
 		return true
 	}
 	return false
+}
+func containNoSpace(username []string) bool {
+	for _,letter:= range username {
+		if letter == ""{
+			return false
+		}
+	}
+	return true
+}
+func userNameExist(usernames []string, username string ) bool {
+	for _,compare := range usernames {
+		if compare == username {
+			return true
+		}
+	}
+	return false
+}
+func combine() {
+	if isLongEnough() == true && containNoSpace() == true && userNameExist() == true {
+		
+	}
 }
 
 func main() {
